018ad93eb70d9b3c5eea81677b4976918bdaa6d7
Janne Adolf <janne.adolf@inbo.be>
2025-12-02 17:12:14 +0100

    Adapt plots sampling frame

--- source/sample_design/plot_sample_frame_sample.R
+++ source/sample_design/plot_sample_frame_sample.R
@@ -1,5 +1,8 @@
-maplist_31370 <- readRDS("data/gis/sample/sampleframe_maplist_31370.Rds")
-source("source/sample_design/grts_sample.R")
+if (!exists("mapfile")) mapfile <- "data/gis/sample/sampleframe_maplist_31370.Rds"
+if (!exists("samplingscript")) samplingscript <- "source/sample_design/grts_sample.R"
+
+maplist_31370 <- readRDS(mapfile)
+source(samplingscript)
 
 # ------------
 # outputs are:
@@ -11,8 +14,6 @@ source("source/sample_design/grts_sample.R")
 
 
 # --- visualize sample ---------------------------------------------
-
-# HERE: convert to Pseudo-mercator
 
 # base plot
 plot_base <- ggplot2::ggplot(data = maplist_31370$map_antw) +
@@ -52,28 +53,41 @@ geoms_buffer <- list(
 # plot distribution area
 plot_dist <- plot_base + geoms_dist + plot_names + ggplot2::theme_void()
 
+
 # plot distribution + buffer area
-plot_dist_buff <- plot_base + geoms_dist + geoms_buffer + plot_names +
-  # emphasize dist
-  ggplot2::geom_sf(data = maplist_31370$map_antw_merged,
-                   fill = NA,
-                   color = INBOtheme::vl_black,
-                   linewidth = 0.8)
+make_plot_dist_buff <- function(geoms_dist, geoms_buffer){
+  plot_base + geoms_dist + geoms_buffer + plot_names +
+    # emphasize dist
+    ggplot2::geom_sf(data = maplist_31370$map_antw_merged,
+                     fill = NA,
+                     color = INBOtheme::vl_black,
+                     linewidth = 0.8) +
+    ggplot2::theme_void()
+}
+# without sampling frame
+plot_dist_buff_0 <- make_plot_dist_buff(geoms_dist[[1]], geoms_buffer[[1]])
+# with sampling frame
+plot_dist_buff <- make_plot_dist_buff(geoms_dist, geoms_buffer)
+
 
 # plot distribution area + sample
 plot_dist_sample <- plot_base + geoms_dist + plot_names +
   ggplot2::geom_rect(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf,
                      fill = "white", alpha = .3) +
   ggplot2::geom_sf(data = sample_hab_upd,
-                   color = INBOtheme::vl_lightred,
-                   fill = INBOtheme::vl_lightred) +
+                   color = INBOtheme::inbo_oranje,
+                   fill = INBOtheme::inbo_oranje) +
   ggplot2::theme_void()
-tmp <- mapview::mapview(
-  list(maplist_31370$map_antw_hab, sample_hab_upd),
-  col.regions = list(INBOtheme::vl_yellow, INBOtheme::vl_lightred)
-)
+if (FALSE){
+  tmp <- mapview::mapview(
+    list(maplist_31370$map_antw_hab, sample_hab_upd),
+    col.regions = list(INBOtheme::vl_yellow, INBOtheme::vl_lightred)
+  )
+}
 
 # save pngs for presentation
-library(patchwork)
-if (FALSE) ggplot2::ggsave("media/img/sample_design/plot_dist_sample.png", (plot_dist + plot_dist_sample))
+if (FALSE) {
+  library(patchwork)
+  ggplot2::ggsave("media/img/sample_design/plot_dist_sample.png", (plot_dist + plot_dist_sample))
+}
 
